16a616802ef9e339cd74a04ca5ca65376825fc82
tonyruan2 <tonyr2@illinois.edu>
2019-08-24 19:07:34 -0500

    Bug fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-﻿var user_interacting = false;
+﻿const valid_bg_color = "white";
+const invalid_bg_color = "red";
+var user_interacting = false;
 var cur_box_id = "";
-var valid_bg_color = "white";
-var invalid_bg_color = "red";
 
 var grid = [
     [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN],
@@ -20,7 +20,7 @@ const subgrid_len = grid_len / 3;
 
 // check if a box has a row error
 function has_row_error(box_row, box_col) {
-    var box_val = grid[box_row][box_col];
+    const box_val = grid[box_row][box_col];
     if (isNaN(box_val)) {
         return false;
     }
@@ -39,7 +39,7 @@ function has_row_error(box_row, box_col) {
 
 // check if a box has a column error
 function has_col_error(box_row, box_col) {
-    var box_val = grid[box_row][box_col];
+    const box_val = grid[box_row][box_col];
     if (isNaN(box_val)) {
         return false;
     }
@@ -58,13 +58,13 @@ function has_col_error(box_row, box_col) {
 
 // check if a box has a subgrid error
 function has_subgrid_error(box_row, box_col) {
-    var box_val = grid[box_row][box_col];
+    const box_val = grid[box_row][box_col];
     if (isNaN(box_val)) {
         return false;
     }
 
-    var start_row = box_row - (box_row % subgrid_len);
-    var start_col = box_col - (box_col % subgrid_len);
+    const start_row = box_row - (box_row % subgrid_len);
+    const start_col = box_col - (box_col % subgrid_len);
 
     for (grid_row = start_row; grid_row < start_row + subgrid_len;
         ++grid_row) {
@@ -87,7 +87,7 @@ $(document).ready(function () {
     // given a row, color valid boxes white and invalid boxes red
     function color_row_boxes(row) {
         for (var grid_col = 0; grid_col < grid_len; ++grid_col) {
-            var box_id = 'r' + row + 'c' + grid_col;
+            const box_id = 'r' + row + 'c' + grid_col;
 
             if (isNaN(grid[row][grid_col])) {
                 if (document.getElementById(box_id)
@@ -116,7 +116,7 @@ $(document).ready(function () {
     // given a col, color valid boxes white and invalid boxes red
     function color_col_boxes(col) {
         for (var grid_row = 0; grid_row < grid_len; ++grid_row) {
-            var box_id = 'r' + grid_row + 'c' + col;
+            const box_id = 'r' + grid_row + 'c' + col;
 
             if (isNaN(grid[grid_row][col])) {
                 if (document.getElementById(box_id)
@@ -146,12 +146,11 @@ $(document).ready(function () {
     // and invalid boxes red
     // assumes the boxes in the given row and col have been previously checked
     function color_subgrid_boxes(row, col) {
-        var start_row = row - (row % subgrid_len);
-        var start_col = col - (col % subgrid_len);
-
-        for (grid_row = start_row; grid_row < start_row + subgrid_len;
+        const start_row = row - (row % subgrid_len);
+        const start_col = col - (col % subgrid_len);
+        for (var grid_row = start_row; grid_row < start_row + subgrid_len;
             ++grid_row) {
-            for (grid_col = start_col; grid_col < start_col + subgrid_len;
+            for (var grid_col = start_col; grid_col < start_col + subgrid_len;
                 ++grid_col) {
                 if (row == grid_row) {
                     break;
@@ -161,7 +160,7 @@ $(document).ready(function () {
                     continue;
                 }
 
-                var box_id = 'r' + grid_row + 'c' + grid_col;
+                const box_id = 'r' + grid_row + 'c' + grid_col;
 
                 if (isNaN(grid[grid_row][grid_col])) {
                     if (document.getElementById(box_id)
@@ -169,9 +168,9 @@ $(document).ready(function () {
                         document.getElementById(box_id)
                             .style.backgroundColor = valid_bg_color;
                     }
-                } else if (!has_row_error(grid_row, grid_col)
-                    && !has_col_error(grid_row, grid_col)
-                    && !has_subgrid_error(grid_row, grid_col)) {
+                } else if (!has_subgrid_error(grid_row, grid_col)
+                    && !has_row_error(grid_row, grid_col)
+                    && !has_col_error(grid_row, grid_col)) {
                     if (document.getElementById(box_id)
                         .style.backgroundColor != valid_bg_color) {
                         document.getElementById(box_id)
@@ -198,8 +197,8 @@ $(document).ready(function () {
     // place a value in a box
     function place_value(box, box_val) {
         box.innerHTML = box_val;
-        var box_row = box.id.charAt(1);
-        var box_col = box.id.charAt(3);
+        const box_row = box.id.charAt(1);
+        const box_col = box.id.charAt(3);
         grid[box_row][box_col] = box_val;
         handle_grid_coloring(box_row, box_col);
     }
@@ -215,7 +214,7 @@ $(document).ready(function () {
     $(document).keypress(function (e) {
         if (user_interacting) {
             var box_val = 0;
-            var key = e.keyCode;
+            const key = e.keyCode;
 
             if (key > 48 && key <= 57) { // numbers
                 box_val = key - 48;
@@ -224,9 +223,7 @@ $(document).ready(function () {
             }
             if (box_val) {
                 var box = document.getElementById(cur_box_id);
-                setTimeout(function () {
-                    place_value(box, box_val);
-                }, 250);
+                place_value(box, box_val);
             }
         }
     });
