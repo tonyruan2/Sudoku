5ca3c2c18d74bb94a6f0635c96d2772a817f8d3a
tonyruan2 <tonyr2@illinois.edu>
2019-09-07 12:47:15 -0500

    Add puzzle rotating, reflecting, reassignment, and loading

--- js/main.js
+++ js/main.js
@@ -1,38 +1,101 @@
 ﻿window.onload = function () {
 
     /*
-     * button functionality
+     * Methods for button functionality.
      */
 
-    document.getElementById("load_btn")
-        .addEventListener("click", load_puzzle, false);
+    document.getElementById("easy_puzzle_btn")
+        .addEventListener("click", load_easy_puzzle, false);
+    document.getElementById("medium_puzzle_btn")
+        .addEventListener("click", load_medium_puzzle, false);
+    document.getElementById("hard_puzzle_btn")
+        .addEventListener("click", load_hard_puzzle, false);
+
     document.getElementById("reset_btn")
         .addEventListener("click", reset_puzzle, false);
-    document.getElementById("undo_btn")
-        .addEventListener("click", undo_last, false);
-    document.getElementById("hint_btn")
-        .addEventListener("click", give_hint, false);
+    document.getElementById("clear_btn")
+        .addEventListener("click", clear_grid, false);
     document.getElementById("solve_btn")
         .addEventListener("click", solve_puzzle, false);
 
     /*
-     * puzzle storage
+     * Information for puzzle storage.
      */
 
-    let easy_puzzles = [
+    const empty_box_val = "_";
 
+    const easy_puzzles = [
+        "1__9_4_____4_3_5_6_2_7__49_____1_94__59_7_32__17_2_____83__7_5_5_1_9_6_____6_5__3"
     ];
 
-    let medium_puzzles = [
-
+    const medium_puzzles = [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
     ];
 
-    let hard_puzzles = [
-
+    const hard_puzzles = [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
     ];
 
     /*
-     * sudoku grid data
+     * Information for the sudoku grid.
      */
 
     function create_Box() {
@@ -55,13 +118,13 @@
         }
     }
 
-    const grid_len = 9;
-    const subgrid_len = grid_len / 3;
+    const grid_length = 9;
+    const subgrid_length = grid_length / 3;
 
     let grid = [];
-    for (let grid_row = 0; grid_row < grid_len; ++grid_row) {
+    for (let grid_row = 0; grid_row < grid_length; ++grid_row) {
         let row = [];
-        for (let grid_col = 0; grid_col < grid_len; ++grid_col) {
+        for (let grid_col = 0; grid_col < grid_length; ++grid_col) {
             row.push(create_Box());
         }
         grid.push(row);
@@ -70,12 +133,16 @@
     const valid_bg_color = "white";
     const invalid_bg_color = "red";
     const empty_text_content = "";
+    const default_box_text_color = "black";
+    const loaded_box_text_color = "blue";
+    const empty_puzzle
+        = "_________________________________________________________________________________";
     let user_interacting = false;
-    let cur_box_id = "";
-
+    let current_box_id = "";
+    let current_puzzle = empty_puzzle;
 
     /*
-     * Error checking for boxes
+     * Methods for error checking boxes.
      */
 
     // determine if a box is modifiable
@@ -92,7 +159,7 @@
             return false;
         }
 
-        for (let grid_col = 0; grid_col < grid_len; ++grid_col) {
+        for (let grid_col = 0; grid_col < grid_length; ++grid_col) {
             if (box_col == grid_col) {
                 continue;
             }
@@ -111,7 +178,7 @@
             return false;
         }
 
-        for (let grid_row = 0; grid_row < grid_len; ++grid_row) {
+        for (let grid_row = 0; grid_row < grid_length; ++grid_row) {
             if (box_row == grid_row) {
                 continue;
             }
@@ -130,12 +197,12 @@
             return false;
         }
 
-        const start_row = box_row - (box_row % subgrid_len);
-        const start_col = box_col - (box_col % subgrid_len);
+        const start_row = box_row - (box_row % subgrid_length);
+        const start_col = box_col - (box_col % subgrid_length);
 
-        for (let grid_row = start_row; grid_row < start_row + subgrid_len;
+        for (let grid_row = start_row; grid_row < start_row + subgrid_length;
             ++grid_row) {
-            for (let grid_col = start_col; grid_col < start_col + subgrid_len;
+            for (let grid_col = start_col; grid_col < start_col + subgrid_length;
                 ++grid_col) {
                 if (box_row == grid_row && box_col == grid_col) {
                     continue;
@@ -150,7 +217,7 @@
     }
 
     /*
-     * Coloring of valid and invalid boxes
+     * Methods for coloring boxes.
      */
 
     // color a box
@@ -164,7 +231,7 @@
 
     // given a row, color valid boxes white and invalid boxes red
     function color_row_boxes(row) {
-        for (let grid_col = 0; grid_col < grid_len; ++grid_col) {
+        for (let grid_col = 0; grid_col < grid_length; ++grid_col) {
             const box_id = 'r' + row + 'c' + grid_col;
 
             if (isNaN(grid[row][grid_col].value())) {
@@ -181,7 +248,7 @@
 
     // given a col, color valid boxes white and invalid boxes red
     function color_col_boxes(col) {
-        for (let grid_row = 0; grid_row < grid_len; ++grid_row) {
+        for (let grid_row = 0; grid_row < grid_length; ++grid_row) {
             const box_id = 'r' + grid_row + 'c' + col;
 
             if (isNaN(grid[grid_row][col].value())) {
@@ -200,11 +267,11 @@
     // and invalid boxes red
     // assumes the boxes in the given row and col have been previously checked
     function color_subgrid_boxes(row, col) {
-        const start_row = row - (row % subgrid_len);
-        const start_col = col - (col % subgrid_len);
-        for (let grid_row = start_row; grid_row < start_row + subgrid_len;
+        const start_row = row - (row % subgrid_length);
+        const start_col = col - (col % subgrid_length);
+        for (let grid_row = start_row; grid_row < start_row + subgrid_length;
             ++grid_row) {
-            for (let grid_col = start_col; grid_col < start_col + subgrid_len;
+            for (let grid_col = start_col; grid_col < start_col + subgrid_length;
                 ++grid_col) {
                 if (row == grid_row) {
                     break;
@@ -250,19 +317,19 @@
     }
 
     /*
-     * user interface methods
+     * Methods for user interaction.
      */
 
     // update that the user is interacting when a box is hovered
     $('.box').hover(function () {
         if (is_modifiable(this)) {
             user_interacting = true;
-            cur_box_id = this.id;
+            current_box_id = this.id;
         }
     }, function () {
             if (is_modifiable(this)) {
                 user_interacting = false;
-                cur_box_id = "";
+                current_box_id = "";
             }
     });
 
@@ -278,55 +345,199 @@
                 box_val = key - 96;
             }
             if (box_val) {
-                let box = document.getElementById(cur_box_id);
+                let box = document.getElementById(current_box_id);
                 place_value(box, box_val);
             }
         }
     });
 
-    /*
-     * button onclick methods 
+    /**
+     * Methods for loading in puzzles.
      */
 
-    // clear the grid and make all boxes empty
-    function clear_grid() {
-        for (let grid_row = 0; grid_row < grid_len; ++grid_row) {
-            for (let grid_col = 0; grid_col < grid_len; ++grid_col) {
+    // rotate the puzzle 90 degrees clockwise 
+    // for a given amount of times
+    function rotate_puzzle(puzzle) {
+        let total_rotations = Math.floor(Math.random() * 4);
+        let current_rotation = 0;
+        let rotated_puzzle = puzzle;
+
+        while (current_rotation != total_rotations) {
+            let previous_puzzle = rotated_puzzle;
+            rotated_puzzle = "";
+            for (let grid_row = 0; grid_row < grid_length; ++grid_row) {
+                for (let grid_col = 0; grid_col < grid_length; ++grid_col) {
+                    rotated_puzzle += previous_puzzle.charAt
+                        (((grid_length - grid_col - 1) * grid_length)
+                            + grid_row);
+                }
+            }
+            ++current_rotation;
+        }
+        return rotated_puzzle;
+    }
+
+    // reflect a puzzle over its horizontal mid-line
+    function reflect_horizontal(puzzle) {
+        let reflected_puzzle = "";
+        for (let grid_row = 0; grid_row < grid_length; ++grid_row) {
+            for (let grid_col = 0; grid_col < grid_length; ++grid_col) {
+                reflected_puzzle += puzzle.charAt
+                    (((grid_length - grid_row - 1) * grid_length)
+                        + grid_col);
+            }
+        }
+        return reflected_puzzle;
+    } 
+
+    // reflect a puzzle over its vertical mid-line
+    function reflect_vertical(puzzle) {
+        let reflected_puzzle = "";
+        for (let grid_row = 0; grid_row < grid_length; ++grid_row) {
+            for (let grid_col = 0; grid_col < grid_length; ++grid_col) {
+                reflected_puzzle += puzzle.charAt
+                    ((grid_row * grid_length) + (grid_length - grid_col - 1));
+            }
+        }
+        return reflected_puzzle;
+    }
+
+    // reflect a puzzle with itself, over its horizontal mid-line,
+    // over its vertical mid-line, or over both of its mid-lines
+    function reflect_puzzle(puzzle) {
+        let reflection_specifier = Math.floor(Math.random() * 4);
+        let reflected_puzzle = puzzle;
+
+        if (reflection_specifier == 1) {
+            reflected_puzzle = reflect_horizontal(puzzle);
+        } else if (reflection_specifier == 2) {
+            reflected_puzzle = reflect_vertical(puzzle);
+        } else if (reflection_specifier == 3) {
+            reflected_puzzle = reflect_horizontal(puzzle);
+            reflected_puzzle = reflect_vertical(reflected_puzzle);
+        }
+        return reflected_puzzle;
+    }
+
+    // reassign each value on the board to another value
+    function reassign_values(puzzle) {
+        let assignments = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        assignments.sort(function (a, b) { return 0.5 - Math.random() });
+        let reassigned_puzzle = "";
+        for (let box_i = 0; box_i < puzzle.length; ++box_i) {
+            if (puzzle.charAt(box_i) == empty_box_val) {
+                reassigned_puzzle += empty_box_val;
+            } else {
+                reassigned_puzzle += assignments[Number(puzzle.charAt(box_i)) - 1];
+            }
+        }
+        return reassigned_puzzle;
+    }
+
+    // scramble a puzzle
+    function scramble_puzzle(puzzle) {
+        let transformed_puzzle = rotate_puzzle(puzzle);
+        transformed_puzzle = reflect_puzzle(transformed_puzzle);
+        transformed_puzzle = reassign_values(transformed_puzzle);
+        return transformed_puzzle;
+    }
+
+    // select a puzzle
+    function select_puzzle(set_of_puzzles) {
+        while (true) {
+            let random_index = Math.floor(Math.random() * (set_of_puzzles.length));
+            selected_puzzle = set_of_puzzles[random_index];
+            if (selected_puzzle != current_puzzle) {
+                break;
+            }
+        }
+        return selected_puzzle;
+    }
+
+    // load a puzzle into the grid
+    function load_puzzle_to_grid(puzzle) {
+        current_puzzle = puzzle;
+        for (let grid_row = 0; grid_row < grid_length; ++grid_row) {
+            for (let grid_col = 0; grid_col < grid_length; ++grid_col) {
                 let box = document
                     .getElementById('r' + grid_row + 'c' + grid_col);
-                grid[grid_row][grid_col].set_modifiable(true);
-                place_value(box, empty_text_content);
+                let puzzle_box_val = puzzle
+                    .charAt((grid_row * grid_length) + grid_col);
+
+                // representation of empty box val in storage is "_"
+                // representation of empty box val on the viewed board is ""
+                // set "_" to "" for placement on the board
+                if (puzzle_box_val == empty_box_val) {
+                    puzzle_box_val = empty_text_content;
+                }
+
+                place_value(box, puzzle_box_val);
+
+                if (puzzle_box_val != empty_text_content) {
+                    box.style.color = loaded_box_text_color;
+                    grid[grid_row][grid_col].set_modifiable(false);
+                } else {
+                    grid[grid_row][grid_col].set_modifiable(true);
+                }
             }
         }
     }
 
-    // load a puzzle with the selected difficulty
-    function load_puzzle() {
+    /*
+    * Methods tied to buttons.
+    */
+
+    // clear the grid and make all boxes empty
+    function clear_grid() {
+        for (let grid_row = 0; grid_row < grid_length; ++grid_row) {
+            for (let grid_col = 0; grid_col < grid_length; ++grid_col) {
+                let box = document
+                    .getElementById('r' + grid_row + 'c' + grid_col);
+                grid[grid_row][grid_col].set_modifiable(true);
+                place_value(box, empty_text_content);
+                if (box.style.color != default_box_text_color) {
+                    box.style.color = default_box_text_color;
+                }
+            }
+        }
+        current_puzzle = empty_puzzle;
+    }
+
+    // load an easy puzzle
+    function load_easy_puzzle() {
         clear_grid();
-        // check difficulty
-        // get a random number limited by size of amount of puzzles
-        // get index of puzzle
-        // loop through string representing puzzle
-        // place values down
-        // make those boxes unmodifiable
+        let default_puzzle = select_puzzle(easy_puzzles);
+        let scrambled_puzzle = scramble_puzzle(default_puzzle);
+        load_puzzle_to_grid(scrambled_puzzle);
+    }
+
+    // load a medium puzzle
+    function load_medium_puzzle() {
+        clear_grid();
+        let default_puzzle = select_puzzle(medium_puzzles);
+        let scrambled_puzzle = scramble_puzzle(default_puzzle);
+        load_puzzle_to_grid(scrambled_puzzle);
+    }
+
+    // load a hard puzzle
+    function load_hard_puzzle() {
+        clear_grid();
+        let default_puzzle = select_puzzle(hard_puzzles);
+        let scrambled_puzzle = scramble_puzzle(default_puzzle);
+        load_puzzle_to_grid(scrambled_puzzle);
     }
 
     // reset a puzzle to the default state
     function reset_puzzle() {
-        // just change puzzle back to unmodifiable tiles
-    }
-
-    // undo the last value placement
-    function undo_last() {
-        // keep array of actions
-        // cleared when grid is cleared/reset or a new puzzle is loaded
-    }
-
-    // change or place a correct value into a box
-    function give_hint() {
-        // try to solve puzzle using given info
-        // give a random uncleared box (if there are any)
-        // otherwise change status
+        for (let grid_row = 0; grid_row < grid_length; ++grid_row) {
+            for (let grid_col = 0; grid_col < grid_length; ++grid_col) {
+                if (grid[grid_row][grid_col].is_modifiable()) {
+                    let box = document
+                        .getElementById('r' + grid_row + 'c' + grid_col);
+                    place_value(box, empty_text_content);
+                }
+            }
+        }
     }
 
     // solve the puzzle for the player
